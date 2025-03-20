bb0877e3cb200b0a9f0f4fb58381e5534f3f562a
quthery <chiefkeef7317@gmail.com>
2025-03-20 23:17:55 +0300

    mini fix

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,7 +1,14 @@
 package model
 
 import (
+	"fmt"
+  style "scfx/internal/styling"
+	"strconv"
+	"time"
+
 	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/fogleman/ease"
 )
 
 type Model struct {
@@ -66,5 +73,138 @@ func (m Model) View() string {
 	} else {
 		s = chosenView(m)
 	}
-	return mainStyle.Render("\n" + s + "\n\n")
+	return style.MainStyle.Render("\n" + s + "\n\n")
+}
+
+type (
+	tickMsg  struct{}
+	frameMsg struct{}
+)
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(time.Time) tea.Msg {
+		return tickMsg{}
+	})
+}
+
+func frame() tea.Cmd {
+	return tea.Tick(time.Second/60, func(time.Time) tea.Msg {
+		return frameMsg{}
+	})
+}
+
+// Update loop for the first view where you're choosing a task.
+func updateChoices(msg tea.Msg, m Model) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "j", "down":
+			m.Choice++
+			if m.Choice > 3 {
+				m.Choice = 3
+			}
+		case "k", "up":
+			m.Choice--
+			if m.Choice < 0 {
+				m.Choice = 0
+			}
+		case "enter":
+			m.Chosen = true
+			return m, frame()
+		}
+
+	case tickMsg:
+		if m.Ticks == 0 {
+			m.Quitting = true
+			return m, tea.Quit
+		}
+		m.Ticks--
+		return m, tick()
+	}
+
+	return m, nil
+}
+
+// Update loop for the second view after a choice has been made
+func updateChosen(msg tea.Msg, m Model) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case frameMsg:
+		if !m.Loaded {
+			m.Frames++
+			m.Progress = ease.OutBounce(float64(m.Frames) / float64(100))
+			if m.Progress >= 1 {
+				m.Progress = 1
+				m.Loaded = true
+				m.Ticks = 3
+				return m, tick()
+			}
+			return m, frame()
+		}
+
+	case tickMsg:
+		if m.Loaded {
+			if m.Ticks == 0 {
+				m.Quitting = true
+				return m, tea.Quit
+			}
+			m.Ticks--
+			return m, tick()
+		}
+	}
+
+	return m, nil
+}
+
+func choicesView(m Model) string {
+	c := m.Choice
+
+	tpl := "What to do today?\n\n"
+	tpl += "%s\n\n"
+	tpl += "Program quits in %s seconds\n\n"
+	tpl += style.SubtleStyle.Render("j/k, up/down: select") + style.DotStyle +
+		style.SubtleStyle.Render("enter: choose") + style.DotStyle +
+		style.SubtleStyle.Render("q, esc: quit")
+
+	choices := fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		checkbox("Python", c == 0),
+		checkbox("C++", c == 1),
+		checkbox("Golang", c == 2),
+		checkbox("NodeJS/TS", c == 3),
+	)
+
+	return fmt.Sprintf(tpl, choices, style.TicksStyle.Render(strconv.Itoa(m.Ticks)))
+}
+
+// The second view, after a task has been chosen
+func chosenView(m Model) string {
+	var msg string
+	var projectName string = "Hello"
+	var projectType string
+	switch m.Choice {
+	case 0:
+		projectType = style.KeywordStyle.Render("python")
+	case 1:
+		projectType = "C++"
+	case 2:
+		projectType = "Golang"
+	default:
+		projectType = "NodeJS/TS"
+
+	}
+
+	msg = fmt.Sprintf(
+		"Creating a %s project in %s\n\n",
+		"Enter project name:",
+		projectType,
+		projectName,
+	)
+	return msg + "\n\n"
+}
+
+func checkbox(label string, checked bool) string {
+	if checked {
+		return style.CheckboxStyle.Render("[x] " + label)
+	}
+	return fmt.Sprintf("[ ] %s", label)
 }
--- internal/styling/styling.go
+++ internal/styling/styling.go
@@ -1,16 +1,48 @@
 package styling
 
-import "github.com/charmbracelet/lipgloss"
+import (
+  "fmt"
+  "strconv"
+  "github.com/lucasb-eyer/go-colorful"
 
+  "github.com/charmbracelet/lipgloss")
+
+const (
+	progressBarWidth  = 71
+	progressFullChar  = "█"
+	progressEmptyChar = "░"
+	dotChar           = " • "
+)
+
+func colorToHex(c colorful.Color) string {
+	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
+}
+
+func colorFloatToHex(f float64) (s string) {
+	s = strconv.FormatInt(int64(f*255), 16)
+	if len(s) == 1 {
+		s = "0" + s
+	}
+	return
+}
+func makeRampStyles(colorA, colorB string, steps float64) (s []lipgloss.Style) {
+	cA, _ := colorful.Hex(colorA)
+	cB, _ := colorful.Hex(colorB)
+
+	for i := 0.0; i < steps; i++ {
+		c := cA.BlendLuv(cB, i/steps)
+		s = append(s, lipgloss.NewStyle().Foreground(lipgloss.Color(colorToHex(c))))
+	}
+	return
+}
 var (
-	keywordStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
-	subtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
-	ticksStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("79"))
-	checkboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
-	progressEmpty = subtleStyle.Render(progressEmptyChar)
-	dotStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
-	mainStyle     = lipgloss.NewStyle().MarginLeft(2)
+	KeywordStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
+	SubtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+	TicksStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("79"))
+	CheckboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
+	DotStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
+	MainStyle     = lipgloss.NewStyle().MarginLeft(2)
 
 	// Gradient colors we'll use for the progress bar
-	ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
+	Ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
 )
